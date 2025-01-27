9e5c9a121530572d450ec248e33bc030822e88ca
Sandra <sebremnes@gmail.com>
2025-01-27 11:37:12 +0100

    oversatt main (tror jeg)

---
+++ Exercise 3/PR_translated_to_go/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Skal det deklareres her, eller er det definert i en av de inkluderte filene?
+const N_FLOORS = 4
+const N_BUTTONS = 3
+
+// func main() int ?
+func main() {
+	fmt.Println("Started!\n")
+
+	inputPollRate_ms := 25
+	con_load("elevator.con", con_val("inputPollRate_ms", &inputPollRate_ms, "%d"))
+
+	input := elevio_getInputDevice()
+
+	if input.floorSensor() == -1 {
+		fsm_onInitBetweenFloors()
+	}
+
+	for {
+		{ // Request button
+			var prev [N_FLOORS][N_BUTTONS]int
+			for f := 0; f < N_FLOORS; f++ {
+				for b := 0; b < N_BUTTONS; b++ {
+					v := input.requestButton(f, b)
+					if v && v != prev[f][b] {
+						fsm_onRequestButtonPress(f, b)
+					}
+					prev[f][b] = v
+				}
+			}
+		}
+
+		{ // Floor sensor
+			prev := -1
+			f := input.floorSensor()
+			if f != -1 && f != prev {
+				fsm_onFloorArrival(f)
+			}
+			prev = f
+		}
+
+		{ // Timer
+			if timer_timedOut() {
+				timer_stop()
+				fsm_onDoorTimeout()
+			}
+		}
+
+		time.Sleep(time.Duration(inputPollRate_ms) * time.Millisecond)
+	}
+}
