b742b8a9c268728ac1c99ded5058022c47dc8d1f
Kaisaantonsen <kaisaga@stud.ntnu.no>
2025-03-18 14:10:40 +0100

    Får sendt rankMld!!!!!!!!

--- ElevatorProject/network/network_master.go
+++ ElevatorProject/network/network_master.go
@@ -9,9 +9,6 @@ import (
 // Adds the host's connection with the relevant client in the list of active connections
 func (ac *ActiveConnections) AddHostConnection(rank int, conn net.Conn, sendChan chan Message) {
 
-	ac.mutex.Lock()
-	defer ac.mutex.Unlock()
-
 	remoteIP, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
 
 	// Check if a connection already exists with this IP
@@ -31,15 +28,23 @@ func (ac *ActiveConnections) AddHostConnection(rank int, conn net.Conn, sendChan
 
 	fmt.Printf("NewConn. ClientIP: %s, Rank: %d", newConn.ClientIP, newConn.Rank)
 
+	ac.mutex.Lock()
 	ac.conns = append(ac.conns, newConn)
+	ac.mutex.Unlock()
 
 	msg := Message{
-		Type: helloMessage,
+		Type: rankMessage,
 		Target: TargetBackup,
-		Payload: "Hello from master",
+		Payload: rank,
 	}
 
-	fmt.Println("Sending hello message on channel")
+	// msg := Message{
+	// 	Type: helloMessage,
+	// 	Target: TargetBackup,
+	// 	Payload: "Hello from master",
+	// }
+
+	fmt.Println("Sending rank message on channel")
 	sendMessageOnChannel(sendChan, msg)
 }
 
@@ -47,21 +52,12 @@ func (ac *ActiveConnections) AddHostConnection(rank int, conn net.Conn, sendChan
 // Master listenes and accepts connections
 func (ac *ActiveConnections)ListenAndAcceptConnections(port string, sendChan chan Message) {
 
-	ac.mutex.Lock()
-	defer ac.mutex.Unlock()
-
 	ln, _ := net.Listen("tcp", ":"+port)
 
 	for {
 		hostConn, _ := ln.Accept()
-		// send rank
 		rank := len(ac.conns) + 2
-		msg := Message{
-			Type: rankMessage,
-			Target: TargetClient,
-			Payload: rank,
-		}
-		sendMessageOnChannel(sendChan, msg)
+
 		go ac.AddHostConnection(rank, hostConn, sendChan)
 	}
 }
@@ -75,8 +71,7 @@ func (ac *ActiveConnections)MasterSendMessages(sendChan chan Message) {
 		fmt.Println("target: ", msg.Target)
 		switch msg.Target {
 		case TargetBackup: 
-			// need to find the conn object connected to backup
-			ac.mutex.Lock()
+			// Need to find the conn object connected to backup
 			fmt.Println("Backup is target")
 			for i := range ac.conns {
 				if ac.conns[i].Rank == 2 {
@@ -85,22 +80,21 @@ func (ac *ActiveConnections)MasterSendMessages(sendChan chan Message) {
 					break
 				}
 			}
-			ac.mutex.Unlock()
+
 		case TargetElevator:
 			// do something
 		case TargetClient: 
+
 			// do something
 		}
 		
 		if targetConn != nil {
 			encoder := json.NewEncoder(targetConn)
-			for msg := range sendChan {
-				fmt.Println("Sending message:", msg)
-				err := encoder.Encode(msg)
-				if err != nil {
-					fmt.Println("Error encoding message: ", err)
-					return
-				}
+			fmt.Println("Sending message:", msg)
+			err := encoder.Encode(msg)
+			if err != nil {
+				fmt.Println("Error encoding message: ", err)
+				return
 			}
 		} else {
 			// If targetConn is nil, log a message or handle the case
