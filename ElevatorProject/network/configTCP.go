d75cf0556379691aa456667e341380c880cb6884
Sandra <sandrebr@ntnu.no>
2025-03-26 08:03:33 +0100

    fjernet litt greier fra ConfigureTCPConn

--- ElevatorProject/network/configTCP.go
+++ ElevatorProject/network/configTCP.go
@@ -5,6 +5,7 @@ import (
 	"net"
 )
 
+// Usikker på om det faktisk hjalp noe
 func ConfigureTCPConn(conn net.Conn) (*net.TCPConn, error) {
 	tcpConn, ok := conn.(*net.TCPConn)
 	if !ok {
@@ -15,15 +16,6 @@ func ConfigureTCPConn(conn net.Conn) (*net.TCPConn, error) {
 	tcpConn.SetNoDelay(true)        // Disable Nagle's algorithm
 	tcpConn.SetReadBuffer(2500000)  // Increase read buffer
 	tcpConn.SetWriteBuffer(2500000) // Increase write buffer
-	// tcpConn.SetKeepAlive(true)                         // Enable keep-alive
-	// tcpConn.SetKeepAlivePeriod(10 * time.Second)       // Keep-alive interval
-
-	// Set congestion control to BBR (Linux only)
-	// fd, err := tcpConn.File()
-	// if err == nil {
-	// 	defer fd.Close()
-	// 	unix.SetsockoptString(int(fd.Fd()), unix.IPPROTO_TCP, unix.TCP_CONGESTION, "bbr")
-	// }
 
 	fmt.Println("TCP settings applied successfully!")
 	return tcpConn, nil
